891977fa2c8f4c4f93e0c3981b0066c1dddbd22a
shareefsk0001 <shareefsk0001@gmail.com>
2023-01-31 23:40:31 +0530

    auth guard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AuthGuard } from './Guards/auth.guard';
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
-  {path:'dashboard', component:DashboardComponent},
+  {path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard] },
   {path:'', component:LoginComponent},
   {path:'**', component:PagenotfoundComponent}
 ];
