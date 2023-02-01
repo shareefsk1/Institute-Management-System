20aeb61116dc6f4c9fd2d63cc00cd490ec9d1f0e
shareefsk0001 <shareefsk0001@gmail.com>
2023-02-01 11:12:11 +0530

    dashborad implemented with components and button with responsiveness wsa added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllStudentsComponent } from './components/all-students/all-students.component';
+import { CreateStudentComponent } from './components/create-student/create-student.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
@@ -7,7 +9,10 @@ import { AuthGuard } from './Guards/auth.guard';
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
-  {path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard] },
+  {path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard], children:[
+    {path:'createStudent', component:CreateStudentComponent},
+    {path:'allStudents', component:AllStudentsComponent}
+  ] },
   {path:'', component:LoginComponent},
   {path:'**', component:PagenotfoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { FormsModule } from '@angular/forms';
+import { CreateStudentComponent } from './components/create-student/create-student.component';
+import { AllStudentsComponent } from './components/all-students/all-students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    CreateStudentComponent,
+    AllStudentsComponent
   ],
   imports: [
     BrowserModule,
