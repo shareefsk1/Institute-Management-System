a89e216d9c362bead77892c56574f90ac20a6c8b
shareefsk0001 <shareefsk0001@gmail.com>
2023-02-01 12:17:23 +0530

    form implemented with personal details with dashboard.css responsiveness used

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-student.component.css']
 })
 export class CreateStudentComponent {
+
+  public createForm : FormGroup = new FormGroup({
+    name : new FormControl(),
+    gender : new FormControl(),
+    mobile : new FormControl(),
+    email : new FormControl(),
+    batch : new FormControl(),
+  })
+
+  submit(){
+    console.log(this.createForm)
+  }
 
 }
